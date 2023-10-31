c16d4598a06eea2b2bd9087992dbe9214c3b5f17
Daniel Puttmann <d.p.puttmann@amsterdamumc.nl>
2023-10-31 15:30:30 +0100

    Added banner and first time loading of custom files Removed unnecessary library loading

--- analysis/00_setup.R
+++ analysis/00_setup.R
@@ -1,33 +1,52 @@
-if(!require("TableOneDataframe")){
-  if(!require("remotes")) install.packages("remotes")
-  remotes::install_github("aasiyahrashan/TableOneDataframe")
-}
+# ------------------------------------------------------------------------------
+# Benchmarking OMOP - Setup
+#
+# By:
+# Aasiyah Rashan  <aasiyah@nicslk.com>
+# Daniel Puttmann <d.p.puttmann@amstedamumc.nl>
+#
+# Date of last edit: 2023-09-26
+# ------------------------------------------------------------------------------
 
-library(tidyverse)
+# Install github packages -------------------------------------------------
+if (!require("remotes")) install.packages("remotes")
+remotes::install_github("aasiyahrashan/TableOneDataframe")
+remotes::install_github("aasiyahrashan/SeverityScoresOMOP@nice")
+
+# Library loading ---------------------------------------------------------
 library(SeverityScoresOMOP)
 library(TableOneDataframe)
 library(glue)
-library(readxl)
-library(googlesheets4)
-library(DBI)
-library(openxlsx)
 
+# Define benchmark date range ---------------------------------------------
 start_date <- "2020-01-01"
 end_date <- "2023-07-31"
 output_path <- "."
 
-### Create the 'secrets.R' file. It needs to include the following parameters
-### for the OMOP database:
-# driver <- ""
-# host <- ""
-# port <- "" (if applicable)
-# dbname <- ""
-# schema <- ""
-# user <- ""
-# password <- ""
-# dataset_name <- ""
+# Create & Open registry-specific files -----------------------------------
+## Get all variables to do the analysis from 'secrets.R'.
+## If is is not filled in, it will open the file for you.
+conn_parr_path <- glue("{getwd()}/analysis/connection_parameters.R")
+if(!file.exists(secrets_path)) {
+  example_path <- glue("{getwd()}/analysis/example_connection_parameters.R")
+  file.copy(from = example_path,
+            to = conn_parr_path)
+  browseURL(conn_parr_path)
+}
+source("analysis/connection_parameters.R")
 
-source("analysis/secrets.R")
+## Creates and opens a custom *_concepts.csv file from example if nonexistent.
+## Add standardized codes to it for look-up during APACHE II calculation.
+## Be careful to not save with ";" but "," as separator!
+concepts_path <- glue("{getwd()}/analysis/{dataset_name}_concepts.csv")
+if (!file.exists(concepts_path)) {
+  example_path <- glue("{getwd()}/analysis/example_concepts.csv")
+  file.copy(from = example_path,
+            to = conn_parr_path)
+  browseURL(concepts_path)
+}
+
+# Run benchmark analysis scripts ------------------------------------------
 source("analysis/apache_ii_prob_function.R")
 source("analysis/smr_graph_functions.R")
 source("analysis/01_analysis.R")
