e05ebba4dc5d3c5f431fab4721c04a3d0a71d907
Daniel Puttmann <dputtmann@hotmail.com>
2023-10-31 14:51:10 +0100

    Update and rename secrets.R to example_connection_parameters.R

---
+++ analysis/example_connection_parameters.R
@@ -0,0 +1,14 @@
+# ------------------------------------------------------------
+# Connection parameters
+# These parameters are needed to connect to the OMOP database
+# ------------------------------------------------------------
+dataset_name <- ""
+
+driver <- ""
+host <- ""
+port <- ""
+dbname <- ""
+schema <- ""
+
+user <- ""
+password <- ""
