d04ce13c15f2c78799d1f825061501585a034639
Aasiyah Rashan <a.rashan@me.com>
2024-01-03 18:30:56 +0000

    rename to prevent datasets getting confused

--- analysis/CCAA source data analysis/01_source_analysis.R
+++ analysis/CCAA source data analysis/01_source_analysis.R
@@ -9,29 +9,45 @@ column_types <- cols(
   "Admission.snomed_diagnosis_concept_id5" = col_character(),
   "Admission.snomed_diagnosis_concept_id6" = col_character(),
   "Admission.snomed_diagnosis_concept_id7" = col_character(),
-  "Admission.snomed_diagnosis_concept_id8" = col_character(),
   .default = col_guess()
 )
-data <- read_csv("data/CoreForms.csv", col_types = column_types)
+source_data <- read_csv("data/CoreForms.csv", col_types = column_types)
 sari_data <- read_csv("data/SariAdmissionAssessment.csv")
+daily <- read_csv("data/DailyAssessmentl.csv")
+daily <- daily %>%
+  filter(DailyAssessment.date_of_daily_assessment == date_of_admission)
+sari_daily <- read_csv("data/SariDailyAssessment.csv")
+sari_daily <- sari_daily %>%
+  filter(SariDailyAssessment.date_of_sari_daily_assessment == date_of_admission) %>%
+  distinct(patient_id, .keep_all = TRUE)
+
 units_of_measure <- read_csv(glue("{output_path}/data/measures.csv"))
 
-data <- left_join(data, sari_data,
+source_data <- left_join(source_data, sari_data,
+                  by = join_by(patient_id, unitId, hospitalId,
+                               date_of_admission, date_of_admission_hospital,
+                               registry))
+source_data <- left_join(source_data, daily,
+                  by = join_by(patient_id, unitId, hospitalId,
+                               date_of_admission, date_of_admission_hospital,
+                               registry))
+
+source_data <- left_join(source_data, sari_daily,
                   by = join_by(patient_id, unitId, hospitalId,
                                date_of_admission, date_of_admission_hospital,
                                registry))
 
 # Getting diagnosis data and diagnosis coefficents ------------------------
-coef_data <- get_apache_ii_coefficents(data,
+coef_data <- get_apache_ii_coefficents(source_data,
   dataset_name = dataset_name,
   source = "source",
   output_path = output_path
 )
-data <- left_join(data, coef_data, by = "patient_id")
+source_data <- left_join(source_data, coef_data, by = "patient_id")
 
 # Applying exclusion criteria ---------------------------------------------
 # First, calculating variables I need. Some just renames to work with OMOP functions
-data <- data %>%
+source_data <- source_data %>%
   filter(date_of_admission >= start_date & date_of_admission <= end_date) %>%
   mutate(
     care_site_name = unitId,
@@ -46,10 +62,10 @@ data <- data %>%
   )
 
 # Removing patients from ineligible sites.
-data <- apply_ccaa_specific_exclusions(data, output_path)
+source_data <- apply_ccaa_specific_exclusions(source_data, output_path)
 
 # Removing readmissions and patients who wouldn't be in OMOP
-data <- data %>%
+source_data <- source_data %>%
   filter(is.na(Admission.readmission) | Admission.readmission != "Yes") %>%
   # Excluding patients who wouldn't be in OMOP.
   filter(Admission.gender %in% c("Male", "Female")) %>%
@@ -57,7 +73,7 @@ data <- data %>%
 
 # Excluding patients from countries with insufficent contributions.
 patients_per_month_country <-
-  data %>%
+  source_data %>%
   mutate(
     country_fac = factor(country),
     admission_month = factor(lubridate::month(date_of_admission))
@@ -94,14 +110,14 @@ patients_per_month_country <-
   summarise(months_contributed_in_year = sum(contributed))
 
 # Excluding patients with insufficent contributions
-data <- data %>%
+source_data <- source_data %>%
   left_join(patients_per_month_country,
             by = c("country", "admission_year")
   ) %>%
   filter(months_contributed_in_year >= 6 | (months_contributed_in_year >= 3 &
                                               admission_year == 2019))
 # Exclusions based on age and diagnosis
-data <- data %>%
+source_data <- source_data %>%
   filter(Admission.age >= 18) %>%
   filter(
     !grepl("*burn*", primary_diagnosis_name, ignore.case = TRUE),
@@ -113,14 +129,14 @@ data <- data %>%
 
 # Calculating APACHE II --------------------------------
 # Measure units file already downloaded with mapping data. Reading it in and joining
-data <- left_join(data, units_of_measure, by = c("unitId" = "unit_id"))
-data <- unit_conversion_source(admission = data)
-data <- calculate_apache_ii_score_source(data)
-data <- calculate_apache_ii_prob(data)
+source_data <- left_join(source_data, units_of_measure, by = c("unitId" = "unit_id"))
+source_data <- unit_conversion_source(admission = source_data)
+source_data <- calculate_apache_ii_score_source(source_data)
+source_data <- calculate_apache_ii_prob(source_data)
 
 
 # Calculating SMRs --------------------------------------------------------
-smrs_ni <- data %>%
+smrs_ni <- source_data %>%
   group_by(country, admission_year) %>%
   summarise(
     total = sum(!is.na(patient_id)),
@@ -135,29 +151,29 @@ smrs_ni <- data %>%
   ungroup()
 
 # Output tables -----------------------------------------------------------
-output <- make_output_df(data, "admission_year")
+output <- make_output_df(source_data, "admission_year")
 output <- get_count(
-  data, "admission_year", "patient_id",
+  source_data, "admission_year", "patient_id",
   "Number of patients", output
 )
 output <- get_unique_count(
-  data, "admission_year", "country",
+  source_data, "admission_year", "country",
   "Number of countries", output
 )
-output <- get_median_iqr(data, "admission_year", "Admission.age",
+output <- get_median_iqr(source_data, "admission_year", "Admission.age",
                          "Age", output,
                          round = 2
 )
-output <- get_n_percent_value(data, "admission_year", "Admission.gender", "Male",
+output <- get_n_percent_value(source_data, "admission_year", "Admission.gender", "Male",
                               "Male", output,
                               round = 2
 )
 
-output <- get_median_iqr(data, "admission_year",
+output <- get_median_iqr(source_data, "admission_year",
                          "apache_ii_score", "APACHE II score", output,
                          round = 2
 )
-output <- get_median_iqr(data, "admission_year", "apache_ii_prob",
+output <- get_median_iqr(source_data, "admission_year", "apache_ii_prob",
                          "APACHE II probability of mortality", output,
                          round = 2
 )
@@ -173,17 +189,17 @@ names(smr_ni_output) <- names(output)
 output <- rbind(output, smr_ni_output[1, ])
 
 ### Outcomes
-output <- get_n_percent_value(data, "admission_year",
+output <- get_n_percent_value(source_data, "admission_year",
                               "Discharge.discharge_status",
                               "Dead", "ICU mortality",
                               output,
                               round = 2
 )
-output <- get_median_iqr(data, "admission_year", "icu_los",
+output <- get_median_iqr(source_data, "admission_year", "icu_los",
                          "ICU length of stay (Days)", output,
                          round = 2
 )
-output <- get_n_percent_value(data, "admission_year", "hospital_outcome",
+output <- get_n_percent_value(source_data, "admission_year", "hospital_outcome",
                               "Dead", "Hospital mortality",
                               output,
                               round = 2
@@ -196,7 +212,7 @@ setColWidths(wb, "3_source_data_tableone", cols = 1:6, widths = "auto")
 saveWorkbook(wb, "output/01_output.xlsx", overwrite = TRUE)
 
 # Getting availability and range of the physiology components of the APACHE II score.
-availability <- get_physiology_variable_availability_source(data)
+availability <- get_physiology_variable_availability_source(source_data)
 
 # Writing the output out.
 wb <- loadWorkbook("output/01_output.xlsx")
